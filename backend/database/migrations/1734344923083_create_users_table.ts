e9d2c0be05bbb17195895a7849d6e89bed0bb2dc
Josue Ngoma <josuengoma26@gmailcom>
2024-12-17 00:12:07 +0100

    update migration

--- backend/database/migrations/1734344923083_create_users_table.ts
+++ backend/database/migrations/1734344923083_create_users_table.ts
@@ -6,8 +6,10 @@ export default class extends BaseSchema {
   async up() {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id').notNullable()
-      table.string('full_name').nullable()
+      table.string('full_name').notNullable()
+      table.string('phone').notNullable().unique()
       table.string('email', 254).notNullable().unique()
+      table.date('birth_day').nullable()
       table.string('password').notNullable()
 
       table.timestamp('created_at').notNullable()
@@ -16,6 +18,8 @@ export default class extends BaseSchema {
   }
 
   async down() {
-    this.schema.dropTable(this.tableName)
+    this.schema.alterTable(this.tableName, (table) => {
+      table.dropColumn('full_name')
+    })
   }
 }
